9a1d26345c79b6194fb279de984bc69e7cb26273
Johan Enström <johan.enstrom@mail.com>
2024-10-28 16:08:14 +0100

    Shorten text in description to fit container

--- react-site/src/projectsData.js
+++ react-site/src/projectsData.js
@@ -15,18 +15,20 @@ export const projects = [
     {
       name: 'Film Database with EF Code-First and Blazor Admin',
       tools: ['EF Code-First', 'Blazor', 'ASP.NET Core', 'REST API', 'SQL Server'],
-      description: 'In this school assignment, I have developed a film database using EF Code-First,' + 
-      'adding various columns including a Film table with URL links and a SimilarFilms table to link related movies.' +
-      'I have also created an API with a data layer as a service to interact with the database and a server-side Blazor' + 
-      'admin application to perform CRUD operations via the API. This admin application will use HttpClient to call the API and does not require JWT authentication.',
+      description: 'In this school assignment, I developed a film database using EF Code-First,'  +
+                    'adding columns for a Film table with URL links and a SimilarFilms table.' + 
+                    'I created an API with a service layer for database interaction and a Blazor admin app' +
+                    'for CRUD operations via HttpClient, without requiring JWT authentication',
       repoLink: 'https://github.com/EnstromJohan/BlazorProject'
     },
     {
       name: 'Moo Game Refactoring',
       tools: ['C#', 'Dependency Injection', 'Unit Testing', 'Mock Objects', 'Design Patterns'],
-      description: 'I refactored the game "Moo" (similar to Mastermind) to improve code quality and testability by addressing code smells,' + 
-      'improving naming, structuring methods, adding comments, and using design patterns. I separated the layers (UI, logic, statistics)' + 
-      'and used Dependency Injection for assembly. I also wrote tests for the logic using Mock Objects to manage I/O and simulate randomness.',
+      description: 'I refactored the game "Moo" (similar to Mastermind) to enhance code quality' + 
+                    'and testability by addressing code smells, improving naming, structuring methods,' + 
+                    'adding comments, and applying design patterns.' + 
+                    'I separated layers (UI, logic, stats), used Dependency Injection for assembly, ' + 
+                    'and tested logic with Mock Objects to manage I/O and randomness.',
       repoLink: 'https://github.com/EnstromJohan/MooPlayGame'
     },
   ];
